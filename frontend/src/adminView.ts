5b8928a99dad3dc49f2e827de44489a601810142
Max Edlund <maxedlund88@gmail.com>
2023-05-04 14:08:52 +0200

    add admin btns and prepare callbacks

--- frontend/src/adminView.ts
+++ frontend/src/adminView.ts
@@ -1,11 +1,14 @@
-function renderAdminCreateView (){
+export function renderAdminCreateView (){
   const main = document.querySelector('main');
   if (main) {
     main.innerHTML = '';
   }
   const taskTitleElement = createInputElement('Task title');
   const taskDescriptionElement = createInputElement('Task description');
-  main?.append(taskTitleElement, taskDescriptionElement);
+  const addTaskBtn = createAdminBtnElement('Add task', 'addTaskBtn', handleAddTask)
+  const removeTasktBtn = createAdminBtnElement('Remove task', 'removeTaskBtn', handleRemoveTask)
+  const saveSessionBtn = createAdminBtnElement('Save and start Session', 'saveSessionBtn', handleSaveSession)
+  main?.append(taskTitleElement, taskDescriptionElement, addTaskBtn, removeTasktBtn, saveSessionBtn);
 }
 
 function createInputElement(placeholder: string): HTMLInputElement {
@@ -16,3 +19,26 @@ function createInputElement(placeholder: string): HTMLInputElement {
   return inputElement;
 } 
 
+function createAdminBtnElement(innerText: string, id: string, callback: (this: HTMLButtonElement, ev: MouseEvent) => any): HTMLButtonElement {
+  const btn = document.createElement('button');
+  btn.innerText = innerText;
+  btn.classList.add('adminBtn');
+  btn.id = id;
+  btn.addEventListener('click', callback);
+  return btn;
+}
+
+function handleAddTask(this: HTMLButtonElement, ev: MouseEvent): void {
+  ev.preventDefault();
+  console.log('add task'); 
+}
+
+function handleRemoveTask(this: HTMLButtonElement, ev: MouseEvent): void {
+  ev.preventDefault();
+  console.log('remove task'); 
+}
+
+function handleSaveSession(this: HTMLButtonElement, ev: MouseEvent): void {
+  ev.preventDefault();
+  console.log('save session'); 
+}
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,5 @@
 import './style.css'
+import * as adminView from './adminView'
 
-
-
+adminView.renderAdminCreateView();
 
