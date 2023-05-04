174527c0efaa52c7d1c87ed6c0248b9fa05e3334
Jesper Eriksson <84853548+jeppeerixon@users.noreply.github.com>
2023-05-04 12:48:08 +0200

    created basic socket backend connection

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,10 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server, {
+  cors: { origin: '*', methods: ['GET', 'POST'] },
+});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +20,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+let voteResults = [];
+
+io.on('connection', (socket) => {
+    console.log('user connected: ' + socket.id);
+
+    socket.on('votes', (data) => {
+        voteResults.push(data);
+        console.log('Votes: ', voteResults);
+        // console.log("Users: ", users[1].user);
+        io.emit('votes', voteResults);
+    });
+
+});
 
 module.exports = app;
