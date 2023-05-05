2c86a07b94a1d83ffbe146267115ac866b703798
Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
2023-05-05 09:35:30 +0200

    fix connection

--- frontend/src/login.ts
+++ frontend/src/login.ts
@@ -56,7 +56,7 @@ const loginCont = document.getElementById("loginContainer") as HTMLElement;
       const username = usernameInput.value;
       const password = passwordInput.value;
 
-      const response = await fetch("https://localhost:3000/login", {
+      const response = await fetch("http://localhost:3000/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
