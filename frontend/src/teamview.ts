b4b3deb3fd541456d333b4ec670e6cf46833510e
Sporesong <114929415+Sporesong@users.noreply.github.com>
2023-05-05 10:27:22 +0200

    🚧 changed name of socket

--- frontend/src/teamview.ts
+++ frontend/src/teamview.ts
@@ -7,7 +7,7 @@ export function renderTeamView() {
     messageBox.innerText = 
     "Thanks for waiting! We're preparing the next session of Planning Poker and it will be ready to play shortly. Get ready to estimate some stories and have fun collaborating with your team!";
 
-    const users = [];
+    const users = [];//ska flyttas till app.js
     const usersList = document.createElement("ul");
     usersList.classList.add(".usersList");
     //fånga upp users och lägg till i arrayen
@@ -21,7 +21,7 @@ export function renderTeamView() {
     startPageContainer.appendChild(usersList);
     startPageContainer.appendChild(joinButton);
 
-    socket.on("session ready", function activateJoinButton() {
+    socket.on("sessionReady", function activateJoinButton() {
         joinButton.classList.toggle(".joinButtonInactive");
     })
 
@@ -31,3 +31,5 @@ joinButton.addEventListener("click", () => {
     });
     
 };
+
+renderTeamView();
