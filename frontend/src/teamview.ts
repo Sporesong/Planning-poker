2a52645b512d220929bb42b3be004c92c872b200
Sporesong <114929415+Sporesong@users.noreply.github.com>
2023-05-05 09:25:25 +0200

    🚧 added pseudocode comments

--- frontend/src/teamview.ts
+++ frontend/src/teamview.ts
@@ -7,21 +7,22 @@ export function renderTeamView() {
     messageBox.innerText = 
     "Thanks for waiting! We're preparing the next session of Planning Poker and it will be ready to play shortly. Get ready to estimate some stories and have fun collaborating with your team!";
 
+    const users = [];
     const usersList = document.createElement("ul");
     usersList.classList.add(".usersList");
+    //fånga upp users och lägg till i arrayen
     //appenda li elements med users som loggat in
 
     joinButton = document.createElement("button");
-    joinButton.classList.add(".joinButton", ".inactive");
+    joinButton.classList.add(".joinButtonInactive");
 
     const startPageContainer = document.querySelector(".startPageContainer") as HTMLElement;
-    startPageContainer?.appendChild(messageBox);
+    startPageContainer.appendChild(messageBox);
     startPageContainer.appendChild(usersList);
     startPageContainer.appendChild(joinButton);
 
     socket.on("session ready", function activateJoinButton() {
-        joinButton.classList.remove(".inactive");
-        joinButton.classList.add(".active");
+        joinButton.classList.toggle(".joinButtonInactive");
     })
 
     
