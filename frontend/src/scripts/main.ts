44e7bd8cc7f61346334db9787755645f5a44d44e
Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
2023-05-08 13:01:29 +0200

    fix

--- frontend/src/scripts/main.ts
+++ frontend/src/scripts/main.ts
@@ -5,17 +5,15 @@ import { renderAdminCreateView } from './adminView';
 import { renderTeamView } from './teamview';
 
 const loginContainer = document.getElementById("loginContainer") as HTMLElement;
-const cardsLogo = document.getElementById("cardsLogo") as HTMLElement;
 
 const token = localStorage.getItem("token");
 
 renderHeader(); // Render the header initially
 
 if (token) {
-  cardsLogo.style.display = "block"; // Show the PNG image
   loginContainer.innerHTML = ""; // Clear the login container
 } else {
-  renderLogin(loginContainer); // Pass the loginContainer to renderLogin
+  renderLogin(); // Render the login form
 }
 renderAdminCreateView();
 renderTeamView();
