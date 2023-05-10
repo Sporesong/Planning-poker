93e7ea6bb0c85889f54fb205fd48316646f2d55f
Max Edlund <maxedlund88@gmail.com>
2023-05-10 12:12:59 +0200

    add index to taskManager

--- frontend/src/scripts/models/TaskManager.ts
+++ frontend/src/scripts/models/TaskManager.ts
@@ -1,11 +1,15 @@
 export class TaskManager {
   public tasks: Task[] = [];
+  public index = 0;
 
   addTask(task: Task) {
     this.tasks.push(task)
   }
   deleteTask(index: number) {
     this.tasks.splice(index, 1);
+  }
+  updateIndex() {
+    this.index++;
   }
 }
 
